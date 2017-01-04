8718bfedd5488540d470cb14369f8b0203ff3805
Tetiana Bond <tanya.bondareva@gmail.com>
2017-01-04 23:36:45 +0100

    move project

--- project/server_v1/src/main/java/org/noteskeeper/server/Main.java
+++ project/server_v1/src/main/java/org/noteskeeper/server/Main.java
@@ -1,10 +1,10 @@
 package org.noteskeeper.server;
 
 /**
- * Created by ioan on 30.12.2016.
+ * Created by ioan on 04.01.2017.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, World!");
     }
 }
