85ea6e08044384115e17092c486ea054c925ab4f
Tetiana Bond <tanya.bondareva@gmail.com>
2017-01-04 23:17:22 +0100

    create project

---
+++ src/main/java/org/noteskeeper/server/Main.java
@@ -0,0 +1,10 @@
+package org.noteskeeper.server;
+
+/**
+ * Created by ioan on 30.12.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
