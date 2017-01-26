1240672d0d3a8b67ac6dfc356289eef6f6ef5927
Tetiana Bond <tanya.bondareva@gmail.com>
2017-01-26 22:51:58 +0100

    add param

--- src/main/java/org/noteskeeper/server/HelloWorld.java
+++ src/main/java/org/noteskeeper/server/HelloWorld.java
@@ -1,18 +1,17 @@
 package org.noteskeeper.server;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-/**
- * Created by ioan on 18.01.2017.
- */
+
 @Path("/hello")
 public class HelloWorld {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getMessage() {
-        return "Got it!";
+    public String getMessage(@QueryParam("name") String name) {
+        if(name == null)
+            return "Hello, Anonimus!";
+        else
+            return "Hello, "+ name;
     }
 }
