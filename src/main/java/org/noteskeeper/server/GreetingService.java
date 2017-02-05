f2f461e098ee79eb5f2d97eb037b258f1c667f75
Tetiana Bond <tanya.bondareva@gmail.com>
2017-02-05 11:06:29 +0100

    fix bug when spring is null or empty

--- src/main/java/org/noteskeeper/server/GreetingService.java
+++ src/main/java/org/noteskeeper/server/GreetingService.java
@@ -5,8 +5,8 @@ public class GreetingService {
     public GreetingService(){}
 
     public String greet(String name){
-        if (name == null && name.isEmpty()) {
-            return "Hello, Anonymous!";
+        if (name == null || name.isEmpty()) {
+            return "Hello, Anonymous";
         } else {
             return "Hello, " + name;
         }
