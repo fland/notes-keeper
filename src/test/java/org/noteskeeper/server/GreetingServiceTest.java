ab8a21f2951ac43bac01276f00ed55d10d62996a
Ivan Petrenko <ioan.petrenko@gmail.com>
2017-02-05 11:15:21 +0100

    Implemented unit tests over GreetingService
    Added Junit dependency
    Added Idea and target directories to gitignore

---
+++ src/test/java/org/noteskeeper/server/GreetingServiceTest.java
@@ -0,0 +1,26 @@
+package org.noteskeeper.server;
+
+/**
+ * Created by ioan1 on 2/5/2017.
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class GreetingServiceTest {
+
+    private GreetingService service = new GreetingService();
+
+    @Test
+    public void greetGreetsAnonymousWhenPassedNullOrEmptyName() {
+        String expectedGreeting = "Hello, Anonymous!";
+        assertEquals("",expectedGreeting, service.greet(null));
+        assertEquals("",expectedGreeting, service.greet(""));
+    }
+
+    @Test
+    public void greetGreetsUserWhenPassedName() {
+        String expectedGreeting = "Hello, Ivan";
+        assertEquals("",expectedGreeting, service.greet("Ivan"));
+    }
+}
