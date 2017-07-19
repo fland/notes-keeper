b8c6944545a30fbe7e644cdc3d76ee6a5257ff79
Ivan Petrenko <ipetrenko@google.com>
2017-07-19 15:33:43 +0200

    Minor changes
    Removed explicit parameter mention in annotations, usage of
    @RestController annotation instead of @Controller.

--- src/main/java/org/noteskeeper/server/HelloWorldController.java
+++ src/main/java/org/noteskeeper/server/HelloWorldController.java
@@ -1,21 +1,17 @@
 package org.noteskeeper.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping(path="/api")
+@RestController
+@RequestMapping("/api")
 public class HelloWorldController {
 
     @Autowired
     private GreetingService greetingService;
 
-    @GetMapping(path="/hello")
-    public @ResponseBody String getMessage(@RequestParam(required=false) String name) {
+    @GetMapping("/hello")
+    public String getMessage(@RequestParam(required=false) String name) {
         return greetingService.greet(name);
     }
 }
